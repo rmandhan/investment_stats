c2fccfbca73739a0cca7360c9aaae105939d70df
Rakesh Mandhan <rakeshmandhan95@gmail.com>
2020-05-01 21:23:47 -0400

    Basic breakdown of code

--- stock_data_manager/manager.py
+++ stock_data_manager/manager.py
@@ -6,6 +6,8 @@ import logging.handlers
 LOGLEVEL = logging.DEBUG
 LOGDIR = '{}/logs'.format(sys.path[0])
 
+
+
 class StockDataManager:
 
     def __init__(self):
@@ -27,5 +29,18 @@ class StockDataManager:
         logger.addHandler(ch)
         logger.debug('Logger Intialized')
         self.logger = logger
+
+    def run(self):
+        # Read YF Positions and get the positions output
+        # Read Stocks List and get the stock list w/ category
+        # Use these stocks to fetch historical data using tiingo api - tiingo class checks if it needs to pull
+        # Use these stocks to fetch any missing data - let iex ascess the situation
+        # Generate Stock object which contains the stock symbol, maybe metadata, and DayQuote data which has stock data for a date
+        # Generate Positions object which contains the stock symbol, and contains Transaction object which contains the data bought/sold and the quantity, and the price
+        # Return Stocks & Positions
+        return 0
+
+    # Include other helper methods in a different module which can manipulate Stocks & Positions to generate certain data required for any graphs
+
 
 manager = StockDataManager()
